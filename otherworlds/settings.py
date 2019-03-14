005109d780399151022b799ed44ceffed27946fc
Nathen Johns <nathenjohns@yahoo.co.uk>
2019-03-14 18:32:24 +0000

    password reset templates and responsive css

--- otherworlds/settings.py
+++ otherworlds/settings.py
@@ -133,3 +133,17 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+
+
+
+
+
+
+
+
+
+
